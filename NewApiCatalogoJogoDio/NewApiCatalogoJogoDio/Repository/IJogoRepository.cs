9b9aa626adb1114d0cc773bd70f258457c19ae07
Alexandre <alexandrelemos01@gmail.com>
2022-01-08 22:18:20 -0300

    commit Inicial

---
+++ NewApiCatalogoJogoDio/NewApiCatalogoJogoDio/Repository/IJogoRepository.cs
@@ -0,0 +1,18 @@
+﻿using NewApiCatalogoJogoDio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewApiCatalogoJogoDio.Repository
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
---
+++ NewApiCatalogoJogoDio/NewApiCatalogoJogoDio/ViewModel/JogoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewApiCatalogoJogoDio.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+    }
+}
